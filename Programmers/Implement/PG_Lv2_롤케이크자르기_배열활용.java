6dfddf22eeca765b87a52dbeb43a60b22cc6fa54
Juhyeon Lee <gogori6565@naver.com>
2026-04-24 17:39:44 +0900

    🟡PG_Lv2_롤케이크자르기_배열활용

--- Programmers/Implement/PG_Lv2_롤케이크자르기_배열활용.java
+++ Programmers/Implement/PG_Lv2_롤케이크자르기_배열활용.java
@@ -1,5 +1,31 @@
-package Implement;
+// 배열로 풀어보기
 
-public class PG_Lv2_롤케이크자르기_배열활용 {
-
+class Solution {
+    public int solution(int[] topping) {
+        
+        int[] leftArr = new int[10001];
+        int[] rightArr = new int[10001];
+        int leftSize = 0;  // 왼쪽 토핑 종류 수
+        int rightSize = 0; // 오른쪽 토핑 종류 수
+        int resultCnt = 0; // 방법 수(정답)
+        
+        // 1. 배열 초기 상태 (모두 오른쪽에 몰음)
+        for(int top : topping){
+            if(rightArr[top] == 0) rightSize++;
+            rightArr[top]++;
+        }
+        
+        // 2. 슬라이딩 윈도우로 토핑 옮기기
+        for(int i=0; i<topping.length; i++){
+            if(leftArr[topping[i]] == 0) leftSize++;
+            leftArr[topping[i]]++;
+            
+            rightArr[topping[i]]--;
+            if(rightArr[topping[i]] == 0) rightSize--;
+            
+            if(leftSize == rightSize) resultCnt++;
+        }
+        
+        return resultCnt;
+    }
 }
